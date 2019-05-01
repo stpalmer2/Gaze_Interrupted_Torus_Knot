833085b8544af2187b35bd9dc1b1e491859d9640
stpalmer2 <47931329+stpalmer2@users.noreply.github.com>
2019-05-01 21:14:11 +1000

    changed back to user

--- threex/threex-artoolkitsource.js
+++ threex/threex-artoolkitsource.js
@@ -183,7 +183,7 @@ ARjs.Source.prototype._initSourceWebcam = function(onReady, onError) {
                 var userMediaConstraints = {
 			audio: false,
 			video: {
-				facingMode: 'environment',
+				facingMode: 'user',
 				width: {
 					ideal: _this.parameters.sourceWidth,
 					// min: 1024,
